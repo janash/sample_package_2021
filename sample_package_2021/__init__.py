0bc00d9f4e07dfae38f9478a08eede7eb6c934a3
Jessica Nash <janash@vt.edu>
2021-01-11 18:43:11 -0500

    reorganize package structure

--- sample_package_2021/__init__.py
+++ sample_package_2021/__init__.py
@@ -4,7 +4,10 @@ This is a sample repo for the MolSSI Best Practices Workshop - January 2021
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas
+from .visualize import draw_molecule, bond_histogram
+from .molecule import calculate_center_of_mass, calculate_molecular_mass, build_bond_list
+from .measure import calculate_distance, calculate_angle
 
 # Handle versioneer
 from ._version import get_versions
