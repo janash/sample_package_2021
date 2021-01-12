8df14ecec40a4ef43ea60c909ac3706b1ac19044
Jessica Nash <janash@vt.edu>
2021-01-11 21:34:48 -0500

    add more tests

--- sample_package_2021/measure.py
+++ sample_package_2021/measure.py
@@ -5,8 +5,22 @@ Functions related to measurements.
 import numpy as np
 
 def calculate_angle(rA, rB, rC, degrees=False):
-    # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
-    # by setting degrees=True
+    """Calculate the angle between three points. 
+    
+    Parameters
+    ----------
+    rA, rB, rC: np.ndarray
+        The coordinates of each point.
+    
+    degrees: bool, optional
+        Indicates whether the answer should be in degrees (True) or radians (False)
+
+    Returns
+    -------
+    theta: float
+        The angle created by the three points.
+    """
+
     AB = rB - rA
     BC = rB - rC
     theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
